9e23c19950986640d114098613e4ae3f71b8827b
chukwudibarrah <ceebrrh@gmail.com>
2023-01-17 21:06:57 +0000

    Final code and readme updated

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# Module 5 Challenge: Password Generator
+# Password Generator Challenge
 
-## Overview
+## Description
 
-This week’s challenge requires you to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+The goal is to create an application that runs and alerts in a browser and an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. 
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of Password Special Characters from the OWASP Foundation](https://www.owasp.org/index.php/Password_special_characters).
+The password can include numbers, lower and upper case characters and special characters.
 
-## Instructions
-
-The following image shows the web application's appearance and functionality:
+The completed project should look like the image below:
 
 ![password generator demo](./assets/05-javascript-challenge-demo.png)
 
+## Workings
+
+The interface should:
 
 * Generate a password when the button is clicked
   * Present a series of prompts for password criteria
@@ -25,55 +26,6 @@ The following image shows the web application's appearance and functionality:
   * Code should validate for each input and at least one character type should be selected
   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
 
-## Grading Requirements
-
-This challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * The challenge should not produce any errors in the console when you inspect it using Chrome DevTools.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality README file with description, screenshot, and link to deployed application.
-
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
----
+The final project page is here: 
 
 © 2022 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- script.js
+++ script.js
@@ -10,33 +10,160 @@ var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
 
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-  let passCharNo = prompt("How many characters would you like your password to be? (between 10 amd 64")
-  alert("Your selected password length is: " + passCharNo)
   
-}
+  // Declare global variables here
+  let arrayForPassword = []; // array to store password characters
+  let noOfCharacters = 0;
+  let passSpecial = false;
+  let passNum = false;
+  let passLow = false;
+  let passCap = false;
+  let selectedCharsNo = 0;
+  let userInput = " ";
+  let thePassword = ""; // to store password random characters
+  
+  
+  function checkForSpace(str) {
+    return str.indexOf(" ") >= 0; // Check and validate for spaces in password characters
+  }
+  
+  // Function to prompt user for password options
+  function getPasswordOptions() {
+    while (noOfCharacters < 10 || noOfCharacters > 64 || !Number.isInteger(noOfCharacters) || checkForSpace(userInput)) {
+      userInput = prompt("Set the length of your password. Your password length MUST be between 10 and 64."); 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+      noOfCharacters =parseFloat(userInput);
+      console.log("Password length is: " + Number.isInteger(noOfCharacters)); 
+     
+      if (checkForSpace(userInput)){
+        alert("Spaces are NOT allowed. Please remove any spaces and try again.")
+      } else if (!Number.isInteger(noOfCharacters)) {
+        alert("Your input is not a number. Please try again")
+      } else if((noOfCharacters < 10) || (noOfCharacters > 64)) {
+        alert("Your password length MUST be between 10 and 64 characters. Try again.")
+      } else {
+        alert("You have specified " + noOfCharacters + " characters in your password.")
+      }
+    }
+    console.log("There are " + noOfCharacters + " characters in the password");
+  
+    alert("Confirm the character types you want to include in your password.")
+    while(!passSpecial && !passNum && !passLow && !passCap) {
+      passSpecial = confirm("Include special characters in your password?");
+      passNum = confirm("Include numeric characters in your password?");
+      passLow = confirm("Include lowercase characters in your password?");
+      passCap = confirm("Include uppercase characters in your password?");
+      
+      if(!passSpecial && !passNum && !passLow && !passCap) {
+        alert("You did not select a valid chracter type. Please select at least one character type.")
+      }
+    }
+  
+    // Get the number of specified characters to be included
+    if(passSpecial) {
+        selectedCharsNo++;
+        console.log("The password has special characters.");
+    };
+    if(passNum){
+        selectedCharsNo++;
+        console.log("The password has numeric characters.");
+    };
+    if(passLow){
+      selectedCharsNo++;
+      console.log("The password has lower case characters.");
+    };
+    if(passCap){
+      selectedCharsNo++;
+      console.log("The password has upper case characters.");
+    };
+    }
+  
+  // Function for getting a random element from an array
+  function getRandom(arr) {
+    let randomNum = Math.floor(Math.random()*arr.length);
+    return arr[randomNum];
+  }
+  
+  // Combine user-selected characters
+  function getSelectedCharacter() {
+    if(passLow){
+      arrayForPassword =arrayForPassword.concat(lowerCasedCharacters);
+    }
+    if(passCap){
+      arrayForPassword = arrayForPassword.concat(upperCasedCharacters);
+    }
+    if(passNum){
+      arrayForPassword = arrayForPassword.concat(numericCharacters);
+    }
+    if(passSpecial){
+      arrayForPassword = arrayForPassword.concat(specialCharacters);
+    }
+    
+  }
+  
+  // Randomise the password string
+  function makeRandom(passString) {
+    let passArray = passString.split("");
+    for (let x = passArray.length - 1; x > 0; x --) {
+      let y = Math.floor(Math.random() * (x + 1));
+      let z = passArray[x];
+      passArray[x] = passArray[y];
+      passArray[y] = z;
+    }
+    return passArray.join("");
+  }
+  
+   // Function to generate password with user input
+  function generatePassword() {
+    getPasswordOptions();
+    getSelectedCharacter();
+   
+    if (passSpecial) {
+        thePassword += getRandom(specialCharacters);
+      }
+      if (passNum) {
+        thePassword += getRandom(numericCharacters);
+      }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+    if (passLow) {
+    thePassword += getRandom(lowerCasedCharacters);
+    }
+    if (passCap) {
+    thePassword += getRandom(upperCasedCharacters);
+    }
+    for (let x = 0; x < noOfCharacters - selectedCharsNo; x++) {
+    thePassword += getRandom(arrayForPassword);
+    }
+  
+    thePassword = makeRandom(thePassword);
+  
+    return thePassword;
+    }
 
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+    // Reset all values
+  function valuesReset() {
+    noOfCharacters = 0;
+    passLow = false;
+    passCap = false;
+    passNum = false;
+    passSpecial = false;
+    selectedCharsNo = 0;
+    arrayForPassword = [];
+    thePassword ="";
+  }
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
+  
+  // Write password to the #password input
+  function writePassword() {
+    valuesReset(); // reset the values before generate a new password
+  
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
